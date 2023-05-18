ffeb8d0d97b779d80d03ce6a3a0dbcb6f369ad83
unknown <mailfordevensingh@gmail.com>
2023-05-18 10:51:04 +0530

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstLoveCakery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
